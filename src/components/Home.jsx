643a9387de0177c3814c223832ac2aeaede4a7f8
Moses Rametsi <158572657+bblack-jewENT@users.noreply.github.com>
2025-12-06 13:53:27 +0200

    Move image to public.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
       {/* Hero Section */}
       <section
         style={{
-          backgroundImage: "url('https://drive.google.com/file/d/13oiLfeGEY6qma0l0iAH3UYior3f_Rxdg/view?usp=sharing')",
+          backgroundImage: "url('public/church-students1.jpeg')",
           color: "#fff",
           padding: "10rem 0",
           marginTop: "0.8rem",
